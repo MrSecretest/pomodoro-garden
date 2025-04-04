0068cffa1c24e062b46705130681765f6d731f43
olehkulys <kulys.olezik@gmail.com>
2025-04-04 19:24:51 +0300

    add attribution

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -30,6 +30,8 @@ export default function Home() {
   const [restPhasesDone, setRestPhasesDone] = useState<string | null>(null);
 
   const [statsShow, setStatsShow] = useState(false);
+  const [infoShow, setInfoShow] = useState(false);
+
   const plantRefs = Array.from({ length: 4 }, () => createRef<PlantRef>());
   const [windowWidth, setWindowWidth] = useState<number>(0);
   useEffect(() => {
@@ -133,6 +135,10 @@ export default function Home() {
     setStatsShow((prev) => !prev);
   };
 
+  const toggleInfo = () => {
+    setInfoShow((prev) => !prev);
+  };
+
   useEffect(() => {
     let interval: NodeJS.Timeout | null = null;
     if (
@@ -161,7 +167,14 @@ export default function Home() {
     return () => {
       if (interval) clearInterval(interval);
     };
-  }, [audio, currentPhase, handleGrowTomato, onComplete, timerIsActive, timeLeft]);
+  }, [
+    audio,
+    currentPhase,
+    handleGrowTomato,
+    onComplete,
+    timerIsActive,
+    timeLeft,
+  ]);
   const t = useTranslations("");
 
   useEffect(() => {
@@ -170,10 +183,21 @@ export default function Home() {
     setWorkPhasesDone(localStorage.getItem("workPhasesDone") ?? "0");
     setRestPhasesDone(localStorage.getItem("restPhasesDone") ?? "0");
   }, []);
-  
 
   return (
     <div className="home-page-bg">
+      <div className="info-toggle">
+        <RoundButton buttonFunc={toggleInfo}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+          >
+            <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+          </svg>
+        </RoundButton>
+      </div>
       <div className="statistic-toggle">
         <RoundButton buttonFunc={toggleStats}>
           <svg
@@ -224,7 +248,51 @@ export default function Home() {
           </motion.div>
         ) : null}
       </AnimatePresence>
-
+      <AnimatePresence>
+        {infoShow ? (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 100 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.1 }}
+            className="stats-container"
+          >
+            <h1>{t("made by Oleh Kulys")}</h1>
+            <p className="stat-name">attribution</p>
+            <a
+              href="https://www.flaticon.com/free-icons/tomato"
+              title="tomato icons"
+            >
+              Tomato icons created by Vectoricons - Flaticon
+            </a>
+            <a
+              href="https://www.flaticon.com/free-icons/tomato"
+              title="tomato icons"
+            >
+              Tomato icons created by Pixelmeetup - Flaticon
+            </a>{" "}
+            <a
+              href="https://www.flaticon.com/free-icons/flower-pot"
+              title="flower pot icons"
+            >
+              Flower pot icons created by Yogi Aprelliyanto - Flaticon
+            </a>
+            <a href="https://github.com/google/material-design-icons">
+            Icons by Google (Material Icons) — Licensed under Apache License 2.0
+            </a>
+            <RoundButton buttonFunc={toggleInfo}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+              >
+                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+              </svg>
+            </RoundButton>
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
       <div className="half-circle-top">
         <svg
           viewBox="0 0 500 150"
